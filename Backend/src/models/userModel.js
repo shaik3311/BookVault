c12eea89eb2473842f29b843124f2d0ab053df3d
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-07-17 21:49:23 +0530

    User Routes Completed

--- Backend/src/models/userModel.js
+++ Backend/src/models/userModel.js
@@ -14,6 +14,14 @@ const userSchema = mongoose.Schema({
     "password":{
         type:String,
         required:true
+    },
+    "role":{
+        type:String,
+        enum:["user","admin"],
+        default : "user"
+    },
+    Bookmarks : {
+        type : mongoose.Schema.Types.ObjectId,
     }
 });
 
